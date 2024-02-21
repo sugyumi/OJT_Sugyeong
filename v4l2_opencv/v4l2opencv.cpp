86d643c4b7d4e41934dd85a849abf835ddbe2420
Sugyeong <tnrld@can-lab.co.kr>
2024-02-21 10:49:22 +0900

    rm gst cmd

--- v4l2_opencv/v4l2opencv.cpp
+++ v4l2_opencv/v4l2opencv.cpp
@@ -1,20 +1,32 @@
-#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
 #include <iostream>
 #include <linux/videodev2.h>
 using namespace cv;
 int main(int ac, char** av) {
-	std::string gst_cmd = "v4l2src ! video/x-raw,format=YUY2,width=640,height=480,framerate=30/1 ! videoconvert ! appsink";
-	//v4l2src ! video/x-raw,format=YUY2,width=640,height=480,framerate=30/1 ! videoconvert ! appsink
-	cv::VideoCapture cap(gst_cmd);
-	cap.open(0, CAP_V4L2);
+	/* "v4l2src ! video/x-raw,format=YUY2,width=640,height=480,framerate=30/1 ! videoconvert ! appsink";
+	std::string device = "/dev/video0"; */
+
+//#define OPTION
+	
+#ifdef OPTION
+	int api = cv::CAP_ANY;
+#else
+	int api = cv::CAP_V4L2;
+#endif
+
+	int deviceID = 0;
+	cv::VideoCapture cap;
+	cap.open(deviceID, api);
 	if (!cap.isOpened())
 	{
 		std::cout << "Can't open the camera" << std::endl;
 		return -1;
 	}
-
 	cv::Mat img;
-	system("date");
+
+#ifdef OPTION
 	while (cap.isOpened())
 	{
 		try
@@ -38,6 +50,19 @@ int main(int ac, char** av) {
 			std::cout << "exception caught: " << err_msg << std::endl;
 		}
 	}
-	system("date");
+#else
+	for (;;)
+	{
+		cap.read(img);
+		if (img.empty()) {
+			std::cerr << "ERROR! blank img\n";
+			break;
+		}
+		imshow("Live", img);
+		if (waitKey(5) >= 0) {
+			break;
+		}
+	}
+#endif
 	return 0;
 }
